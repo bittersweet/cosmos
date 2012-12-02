3b9ffea3b4970fcf4742d801864e484b2d71cf64
Mark Mulder <markmulder@gmail.com>
2012-12-02 12:37:59 +0100

    Add Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cosmos::Application.config.secret_token = 'ad84cab2e19aaf4acfdde878ee448e88f87d3d8eedb9925f059d45f6950cf37ce9437247c9677d65d7cb2fbbfffb93cbf1923cbfe7df19c27e813015f89ee246'
