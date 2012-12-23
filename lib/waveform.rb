8155b5e4987a794a8b4b28671a43c2f8178f7d65
Mark Mulder <markmulder@gmail.com>
2012-12-23 10:50:24 +0100

    Use full path for waveform binary

--- lib/waveform.rb
+++ lib/waveform.rb
@@ -20,7 +20,7 @@ class Waveform
   def generate_waveform
     file = @track.file.path
     filename = Rails.root + "tmp/#{@track.id}-waveform.png"
-    `waveform --width 1800 --height 280 --color-bg 00000000 --color-center 000000ff --color-outer 000000ff #{file} #{filename}`
+    `/usr/local/bin/waveform --width 1800 --height 280 --color-bg 00000000 --color-center 000000ff --color-outer 000000ff #{file} #{filename}`
     output_file = File.open(filename)
     @track.waveform = output_file
     @track.save
